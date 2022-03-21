20b903b48b8684176a148a9e522dca9b474697f1
pradeep2512m <pradeepm6383@gmail.com>
2022-03-21 20:14:09 +0530

    Multiply

---
+++ src/com/harman/project/Multiply.java
@@ -0,0 +1,12 @@
+package com.harman.project;
+
+public class Multiply {
+    public static void main(String[] args) {
+        int x = 12;
+        int y = 13;
+        int z = x * y;
+        System. out. println("Multiply: " + z);
+
+
+    }
+}
