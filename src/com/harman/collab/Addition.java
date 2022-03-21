b575178049ef6506e0acb89cc1a8480963b61892
SreeVarshan18 <varshan.sree18@gmail.com>
2022-03-21 18:49:59 +0530

    Addition function added

---
+++ src/com/harman/collab/Addition.java
@@ -0,0 +1,8 @@
+package com.harman.collab;
+
+public class Addition {
+
+    int addnum(int x , int y){
+        return x+y;
+    }
+}
--- src/com/harman/collab/Demo.java
+++ src/com/harman/collab/Demo.java
@@ -1,4 +1,13 @@
 package com.harman.collab;
 
 public class Demo {
+
+    public static void main(String[] args) {
+
+        int x=10,y=20, sum;
+        Addition add_ob = new Addition();
+        sum = add_ob.addnum(x,y);
+        System.out.println(sum);
+
+    }
 }
